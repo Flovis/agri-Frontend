f4443aaa90c0aac219ff6c43e00f49c0bb09432e
hesney sham <120007802+01herve@users.noreply.github.com>
2023-06-21 11:36:31 +0200

    initial commit

--- src/data/Meteodata.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-import React from "react";
-
-export default function Meteodata() {
-  async function getMeteo() {
-    const response = await axios.get("");
-  }
-  return <div>Meteodata</div>;
-}
