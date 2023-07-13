38e9e482477a53c4621146e6acb9f59067745ab1
hesney sham <120007802+01herve@users.noreply.github.com>
2023-07-13 13:32:29 +0200

    pull adding

---
+++ src/components/dashComponent/header/GestionNotification.js
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+import Notification from "./Notification";
+import { useNavigate } from "react-router-dom";
+
+export default function GestionNotification() {
+  const navigate = new useNavigate();
+
+  const handleClick = () => {
+    navigate("/contenu/notification");
+  };
+  const radios = [
+    {
+      name: "Hobby plan",
+      description: "For personal or non-commercial projects.",
+    },
+    {
+      name: "Pro plan",
+      description: "For team collaboration with advanced features.",
+    },
+    {
+      name: "Enterprise plan",
+      description: "For teams with security,and performance needs.",
+    },
+  ];
+
+  return (
+    <div className=" mx-auto px-4 bg-custom-white">
+      <h2 className="text-gray-800 font-medium">Notification</h2>
+      <ul className="mt-6 space-y-3 overflow-y-auto h-80 ">
+        {radios.map((item, idx) => (
+          <li key={idx} onClick={handleClick}>
+            <label htmlFor={item.name} className="block relative">
+              <input
+                id={item.name}
+                type="radio"
+                defaultChecked={idx == 1 ? true : false}
+                name="payment"
+                class="sr-only peer"
+              />
+              <div className="w-full p-5 cursor-pointer rounded-lg border border-borde-gray bg-white shadow-sm ring-borde-gray peer-checked:ring-2 duration-200">
+                <div className="pl-7">
+                  <h3 className="leading-none text-gray-800 font-medium">
+                    {item.name}
+                  </h3>
+                  <p className="mt-1 text-sm text-gray-600">
+                    {item.description}
+                  </p>
+                </div>
+              </div>
+              <span className="block absolute top-5 left-5 border peer-checked:border-[5px] peer-checked:border-indigo-600 w-4 h-4 rounded-full"></span>
+            </label>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
 import { AuthProvider } from "./context/AuthProvider";
 import { ContentFormProvider } from "./hooks/useDataForm";
+import { SocketProvider } from "./context/SocketContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
